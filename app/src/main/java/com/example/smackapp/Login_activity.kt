c74a90bf05b668832456107779cb087bb60d9312
Adel Karshenas <adel.karshen@gmail.com>
2020-01-02 09:50:58 +0330

    new

--- app/src/main/java/com/example/smackapp/Login_activity.kt
+++ app/src/main/java/com/example/smackapp/Login_activity.kt
@@ -1,12 +1,20 @@
 package com.example.smackapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Login_activity : AppCompatActivity() {
-
+    fun OnSignUp(view: View)
+    {
+        val hadiint=Intent(this,sign_up_activity::class.java)
+        startActivity(hadiint)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_activity)
+
     }
+
 }
